d856f3bbee648f45b9b983e784bcf00735769c02
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-21 01:43:42 +0100

    feat:add function to define the path of folders and files

---
+++ src/utils/defineProjectPaths.js
@@ -0,0 +1,33 @@
+export const defineProjectPaths = (projectName) => {
+    const dir = projectName;
+    const src = `${dir}/src`;
+    const configPath = `${src}/config`;
+    const viewsPath = `${src}/views`;
+    const modelsPath = `${src}/models`;
+    const controllersPath = `${src}/controllers`;
+    const repositoriesPath = `${src}/repositorie`;
+    const servicesPath = `${src}/service`;
+    const routesPath = `${src}/routes`;
+    const appPath = `${dir}/app.js`;
+    const serverPath = `${dir}/server.js`;
+    const gitIgnorePath = `${dir}/.gitignore`;
+    const envPath = `${dir}/.env`;
+    const envExemplePath = `${dir}/.env.exemple`;
+    const readmePath = `${dir}/README.md`;
+
+    return {
+        configPath,
+        viewsPath,
+        modelsPath,
+        controllersPath,
+        repositoriesPath,
+        servicesPath,
+        routesPath,
+        appPath,
+        serverPath,
+        gitIgnorePath,
+        envPath,
+        envExemplePath,
+        readmePath,
+    };
+};
