86d85703fbf4b899e1ca8db71bad3b488785bba2
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-22 22:46:18 +0100

    feat:resolve path template

---
+++ src/utils/resolvePath.js
@@ -0,0 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const resolvePathTemplate = (...segments) => {
+    return resolve(__dirname, '..', '..', 'templates', ...segments);
+};
