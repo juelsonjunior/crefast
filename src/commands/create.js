d5cd05c582de8dc68fdd082a59ad8330257e5b0a
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-22 01:48:09 +0100

    refactor: ajust name project and add comand create

--- src/commands/create.js
+++ src/commands/create.js
@@ -2,22 +2,21 @@ import { createAnswers } from '../utils/index.js';
 
 export const createCommand = (program) => {
     program
-        .name('create [project_name]')
+        .command('create [project_name]')
         .description('Criar um projeto')
         .option('-n, --name <name>', 'Nome do projeto')
         .option(
             '-t, --structure <type>',
-            'Tipo de estrutura (REST, MVC, MODULAR)',
-            'REST'
+            'Tipo de estrutura (REST, MVC, MODULAR)'
         )
         .option('--no-git', 'Não inicializa o projeto git')
         .option('--no-dependences', 'Não instala as dependencias')
         .action(async (projectName, options) => {
-            if (projectName) {
-                options.project_name = projectName;
+            const name = options.name || projectName;
+            if (name) {
+                options.name = name;
             }
 
             await createAnswers(options);
         });
 };
-
