99017777961c5be54c002302af284fc2479895cd
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-23 06:18:56 +0100

    feat: expot new function resolvePathPackage

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,5 +3,6 @@ export { generateTemplateFile } from './generateTemplateFile.js';
 export { mergeAnswers } from './mergeAnswers.js';
 export { resolveFolderConflict } from './resolveFolderConflict.js';
 export { runWithSpinner } from './runWithSpinner.js';
-export { resolvePathTemplate } from './resolvePath.js';
+export { resolvePathTemplate, resolvePathPackage } from './resolvePath.js';
 export { createFromTemplate } from './createFromTemplate.js';
+export { errorHandler } from './errorHandler.js';
