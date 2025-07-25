c88d09a772ffe9469a006141e11fb6ca12e2d9e1
Juelson Junior <juelsonjunior27@gmail.com>
2025-07-25 08:01:49 +0100

    refcator:add templates in structure modular

--- src/core/generator/createStructureModular.js
+++ src/core/generator/createStructureModular.js
@@ -1,69 +1,167 @@
-import chalk from 'chalk';
+import fs from 'fs/promises';
 
 import {
     createIfNotExists,
     initializeProject,
     saveCliMetadata,
 } from '../../core/index.js';
-import { resolveFolderConflict } from '../../utils/resolveFolderConflict.js';
-import { runWithSpinner } from '../../utils/runWithSpinner.js';
+import {
+    createFromTemplate,
+    resolvePathTemplate,
+    errorHandler,
+    resolveFolderConflict,
+} from '../../utils/index.js';
+import { structureBuilder } from './structureBuilder.js';
 
 export const createStructureModular = async (answers) => {
-    const { safeName, canceled, paths } = await resolveFolderConflict(
-        answers.safeName
-    );
-
-    if (canceled) return false;
-
-    answers.safeName = safeName;
-
     try {
-        await runWithSpinner('Criando pasta controllers', async () => {
-            await createIfNotExists(paths.controllersPath, 'dir');
-        });
-        await runWithSpinner('Criando pasta service', async () => {
-            await createIfNotExists(paths.servicesPath, 'dir');
-        });
-        await runWithSpinner('Criando pasta repositorie', async () => {
-            await createIfNotExists(paths.repositoriesPath, 'dir');
-        });
-        await runWithSpinner('Criando pasta models', async () => {
-            await createIfNotExists(paths.modelsPath, 'dir');
-        });
-        await runWithSpinner('Criando pasta routes', async () => {
-            await createIfNotExists(paths.routesPath, 'dir');
-        });
-        await runWithSpinner('Criando arquivo app.js', async () => {
-            await createIfNotExists(
-                paths.appPath,
-                'file',
-                `console.log("APP construido")`
-            );
-        });
-        await runWithSpinner('Criando arquivo server.js', async () => {
-            await createIfNotExists(
-                paths.serverPath,
-                'file',
-                `console.log("SERVER construido")`
-            );
-        });
-        await runWithSpinner('Criando arquivo README.md', async () => {
-            await createIfNotExists(
-                paths.readmePath,
-                `# ${answers.safeName}\n README montado na minha CLI`
-            );
-        });
+        const { safeName, canceled, paths } = await resolveFolderConflict(
+            answers.safeName
+        );
 
-        await initializeProject(paths.dir, answers);
+        if (canceled) return false;
+        answers.safeName = safeName;
 
-        await runWithSpinner('Salvando metadados do projeto', async () => {
-            await saveCliMetadata(paths.dir, answers);
-        });
+        // Garante que a pasta raiz do projeto existe
+        await fs.mkdir(paths.dir, { recursive: true });
+
+        const folders = [
+            {
+                name: 'controllers',
+                template: 'baseController.ejs',
+                fileName: 'baseController.js',
+            },
+            {
+                name: 'repositories',
+                template: 'baseRepositorie.ejs',
+                fileName: 'baseRepositorie.js',
+            },
+            {
+                name: 'routes',
+                template: 'baseRoute.ejs',
+                fileName: 'baseRoute.js',
+            },
+            {
+                name: 'services',
+                template: 'baseService.ejs',
+                fileName: 'baseService.js',
+            },
+        ];
+
+        const steps = [
+            ...folders.map(({ name, template, fileName }) => ({
+                label: '',
+                action: async ({ paths }) => {
+                    const foldePath = paths[`${name}Path`];
+                    await createIfNotExists(foldePath);
+                    await createFromTemplate(
+                        resolvePathTemplate(`modular/${name}`, template),
+                        `${foldePath}/${fileName}`
+                    );
+                    return `${foldePath}/${fileName}`;
+                },
+            })),
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('modular/', 'app.ejs'),
+                        paths.appPath
+                    );
+                    return paths.appPath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('modular/', 'server.ejs'),
+                        paths.serverPath
+                    );
+                    return paths.serverPath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', 'README.ejs'),
+                        paths.readmePath,
+                        { projectName: answers.safeName }
+                    );
+                    return paths.readmePath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', '.gitignore.ejs'),
+                        paths.gitIgnorePath
+                    );
+                    return paths.gitIgnorePath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', '.prettier.ejs'),
+                        paths.prettierPath
+                    );
+                    return paths.prettierPath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', 'eslintConfig.ejs'),
+                        paths.eslintPath
+                    );
+                    return paths.eslintPath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', '.env.ejs'),
+                        paths.envPath
+                    );
+                    return paths.envPath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths }) => {
+                    await createFromTemplate(
+                        resolvePathTemplate('', '.envExemple.ejs'),
+                        paths.envExemplePath
+                    );
+                    return paths.envExemplePath;
+                },
+            },
+            {
+                label: '',
+                action: async ({ paths, answers }) => {
+                    const metadataPath = await saveCliMetadata(
+                        paths.dir,
+                        answers
+                    );
+                    return metadataPath;
+                },
+            },
+        ];
+
+        console.log();
+        await structureBuilder(steps, { paths, answers });
+        console.log();
+        await initializeProject(paths.dir, paths.serverPath, answers);
+
         return true;
     } catch (err) {
-        console.error(
-            chalk.red('❌ Erro ao criar a estrutura MODULAR', err.message)
-        );
+        errorHandler('Erro ao criar a estrutura MODULAR', err);
         return false;
     }
 };
