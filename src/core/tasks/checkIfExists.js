cb19babd2f0af4ea11e2e9dec792873bc601516d
juelsonjunior <juelsonjunior27@gmail.com>
2025-08-26 23:22:21 +0100

    feat:show error in checkIfExist

--- src/core/tasks/checkIfExists.js
+++ src/core/tasks/checkIfExists.js
@@ -5,8 +5,8 @@ export const checkIfExist = async (path) => {
     try {
         await access(path, constants.F_OK);
         return true;
-        // eslint-disable-next-line no-unused-vars
     } catch (err) {
+        console.log(err);
         return false;
     }
 };
