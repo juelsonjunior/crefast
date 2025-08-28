8a7c686314c522e325c021b7ffa1ff845920e88a
juelsonjunior <juelsonjunior27@gmail.com>
2025-08-28 14:20:02 +0100

    style:ajust style message

--- src/utils/finishMessage.js
+++ src/utils/finishMessage.js
@@ -3,16 +3,16 @@ import { t } from '../i18n/index.js';
 
 export const finishMessage = (projectName) => {
     const msg = `
-    🚀' ${t('project.created.success')} ${chalk.green(projectName)}
+🚀 ${t('project.created.success')} ${chalk.green(projectName)}
 
-    ${t('project.created.steps')}
-    1. ${chalk.gray(`📂 cd ${projectName}`)}
-    2. ${chalk.gray('🛠️  npm run dev')}
+${t('project.created.steps')}
+1. ${chalk.gray(`📂 cd ${projectName}`)}
+2. ${chalk.gray('🛠️  npm run dev')}
 
-                ${chalk.yellow('✨' + t('project.created.thanks'))}
+        ${chalk.yellow(`✨ ${t('project.created.thanks')}`)}
 
-    ${chalk.white(`${chalk.bold('👉' + t('project.created.fork'))} https://github.com/juelsonjunior/nodegen`)}
-    ${chalk.white(`${chalk.bold('💬' + t('project.created.networking'))} https://www.linkedin.com/in/juelson-júnior-5b4974310`)}
+${chalk.white(`${chalk.bold(`👉 ${t('project.created.fork')}`)} https://github.com/juelsonjunior/nodegen`)}
+${chalk.white(`${chalk.bold(`💬 ${t('project.created.networking')}`)} https://www.linkedin.com/in/juelson-júnior-5b4974310`)}
     `;
 
     console.log(msg);
