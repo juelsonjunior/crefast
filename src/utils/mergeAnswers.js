f07ff2d04ad234acd34cb77dd526be9ccd90f621
juelsonjunior <juelsonjunior27@gmail.com>
2025-08-30 04:51:42 +0100

    refactor:add package in pameter function finishMessage

--- src/utils/mergeAnswers.js
+++ src/utils/mergeAnswers.js
@@ -43,7 +43,7 @@ export const mergeAnswers = async (options) => {
         if (!success) {
             throw new Error(t('error.creation.failed'));
         }
-        finishMessage(name);
+        finishMessage(name, packageManager);
     } catch (error) {
         errorHandler(t('error.catch.handler'), error);
     }
