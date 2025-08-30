892406ae490427bc40ce74f42b0e530c1f51839b
juelsonjunior <juelsonjunior27@gmail.com>
2025-08-30 03:49:01 +0100

    refactor: install depences based in package manager

--- src/core/tasks/initializeProject.js
+++ src/core/tasks/initializeProject.js
@@ -1,28 +1,67 @@
 import { execa } from 'execa';
 import { editPackageJson } from './editPackageJson.js';
 import { runWithSpinner } from '../../utils/runWithSpinner.js';
-import { errorHandler } from '../../utils/index.js';
+import { errorHandler, validatePackageManager } from '../../utils/index.js';
 import { t } from '../../i18n/index.js';
 
+const packageManagerConfig = {
+    npm: {
+        installCommand: 'install',
+        devFlag: '--save-dev',
+        initCommand: 'init',
+        initArgs: ['-y'],
+    },
+    yarn: {
+        installCommand: 'add',
+        devFlag: '-D',
+        initCommand: 'init',
+        initArgs: ['-y'],
+    },
+    pnpm: {
+        installCommand: 'add',
+        devFlag: '--save-dev',
+        initCommand: 'init',
+        initArgs: ['-y'],
+    },
+};
 export const initializeProject = async (dir, serverBase, answers) => {
+    const packageManager = answers.packageManager;
+
+    const { installCommand, devFlag, initCommand, initArgs } =
+        packageManagerConfig[packageManager];
+
+    const dependencies = ['express', 'dotenv'];
+    const devDependencies = [
+        'nodemon',
+        '@eslint/js',
+        'eslint',
+        'globals',
+        'prettier',
+    ];
+
     try {
         await runWithSpinner(`${t('install.dependences')} ☕`, async () => {
-            await execa('npm', ['init', '-y'], { cwd: dir });
-            await execa('npm', ['install', '-D', 'nodemon'], { cwd: dir });
-            await execa('npm', ['install', 'express'], { cwd: dir });
-            await execa('npm', ['install', 'dotenv'], { cwd: dir });
+            const resultPackage = await validatePackageManager(packageManager);
+
+            console.log("Ver o result", resultPackage);
+            
+            // Executa o comando de inicialização
+            await execa(packageManager, [initCommand, ...initArgs], {
+                cwd: dir,
+            });
+
+            // Instala as dependências de desenvolvimento
             await execa(
-                'npm',
-                [
-                    'install',
-                    '-D',
-                    '@eslint/js',
-                    'eslint',
-                    'globals',
-                    'prettier',
-                ],
+                packageManager,
+                [installCommand, devFlag, ...devDependencies],
                 { cwd: dir }
             );
+
+            // Instala as dependências de produção
+            await execa(packageManager, [installCommand, ...dependencies], {
+                cwd: dir,
+            });
+
             await editPackageJson(dir, serverBase, answers.safeName);
         });
     } catch (err) {
