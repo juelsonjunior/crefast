49158ba6c553e8c80d7435b7eaa5ac606e493abd
juelsonjunior <juelsonjunior27@gmail.com>
2025-09-01 19:12:51 +0100

    refactor:ajust test cliMetadata

--- src/core/metadata/__tests__/cliMetadata.test.js
+++ src/core/metadata/__tests__/cliMetadata.test.js
@@ -3,57 +3,69 @@ import { errorHandler, resolvePathPackage } from '../../../utils/index.js';
 import { readFile, writeFile } from 'fs/promises';
 
 jest.mock('fs/promises', () => ({
-  readFile: jest.fn(),
-  writeFile: jest.fn(),
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
 }));
 
 jest.mock('../../../utils/index.js', () => ({
-  errorHandler: jest.fn(),
-  resolvePathPackage: jest.fn(),
+    errorHandler: jest.fn(),
+    resolvePathPackage: jest.fn(),
 }));
 
 describe('saveCliMetadata', () => {
-  const fakeDir = '/fake/project';
-  const fakeAnswers = { safeName: 'meuApp', structure: 'MVC' };
+    const fakeDir = '/fake/project';
+    const fakeAnswers = {
+        safeName: 'meuApp',
+        structure: 'MVC',
+        use_git: true,
+        use_oop: 'oop',
+        packageManager: 'npm',
+    };
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-    resolvePathPackage.mockReturnValue('/fake/path/to/cli/package.json');
-  });
-
-  test('Salva metadata corretamente', async () => {
-    // Simulando package.json da CLI
-    readFile.mockResolvedValue(JSON.stringify({ version: '1.2.3' }));
-
-    const result = await saveCliMetadata(fakeDir, fakeAnswers);
-
-    expect(readFile).toHaveBeenCalledWith('/fake/path/to/cli/package.json', { encoding: 'utf-8' });
-    expect(writeFile).toHaveBeenCalled();
-
-    const [filePath, writtenContent] = writeFile.mock.calls[0];
-    const parsed = JSON.parse(writtenContent);
-
-    expect(filePath).toBe(`${fakeDir}/.cli-metadata.json`);
-    expect(parsed).toMatchObject({
-      name_project: 'meuApp',
-      structure: 'mvc',
-      cli_verion: '1.2.3',
-      node_verion: process.version,
-      platform: process.platform,
-      authorCLI: 'Juelson Júnior',
+    beforeEach(() => {
+        jest.clearAllMocks();
+        resolvePathPackage.mockReturnValue('/fake/path/to/cli/package.json');
     });
 
-    expect(result).toBe(`${fakeDir}/.cli-metadata.json`);
-  });
+    test('Salva metadata corretamente', async () => {
+        // Simulando package.json da CLI
+        readFile.mockResolvedValue(JSON.stringify({ version: '1.2.3' }));
 
-  test('Chama errorHandler em caso de erro', async () => {
-    readFile.mockRejectedValue(new Error('Falha de leitura'));
+        const result = await saveCliMetadata(fakeDir, fakeAnswers);
 
-    await saveCliMetadata(fakeDir, fakeAnswers);
+        expect(readFile).toHaveBeenCalledWith(
+            '/fake/path/to/cli/package.json',
+            { encoding: 'utf-8' }
+        );
+        expect(writeFile).toHaveBeenCalled();
 
-    expect(errorHandler).toHaveBeenCalledWith(
-      'Não foi possível ler o package.json do CLI',
-      expect.any(Error)
-    );
-  });
+        const [filePath, writtenContent] = writeFile.mock.calls[0];
+        const parsed = JSON.parse(writtenContent);
+
+        expect(filePath).toBe(`${fakeDir}/.cli-metadata.json`);
+        expect(parsed).toMatchObject({
+            name_project: 'meuApp',
+            structure: 'mvc',
+            style_code: 'oop',
+            git_init: true,
+            package_manager: 'npm',
+            cli_verion: '1.2.3',
+            node_verion: process.version,
+            platform: process.platform,
+            cli_author: 'Juelson Júnior',
+        });
+
+        expect(result).toBe(`${fakeDir}/.cli-metadata.json`);
+    });
+
+    test('Chama errorHandler em caso de erro', async () => {
+        readFile.mockRejectedValue(new Error('Falha de leitura'));
+
+        await saveCliMetadata(fakeDir, fakeAnswers);
+
+        expect(errorHandler).toHaveBeenCalledWith(
+            'Não foi possível ler o package.json do CLI',
+            expect.any(Error)
+        );
+    });
 });
